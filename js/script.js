f07dd09ae88e62e8aca1c1397556d34bc07bf844
dianaxio <athina_7777@hotmail.com>
2023-01-04 17:53:56 -0300

    changes

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let d = new Date();
+
+document.getElementById("currentYear").textContent = d.getFullYear();
+
+let lastUpdated = document.lastModified;
+
+document.getElementById("updatedDate").textContent = lastUpdated;
